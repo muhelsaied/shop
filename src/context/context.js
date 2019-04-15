d78d863e0a721fcc25279e2d70323f3bb91393cd
MUHAMED ELSAIED <muhahmed2099@gmail.com>
2019-04-16 00:49:05 +0200

    setLocalstora

--- src/context/context.js
+++ src/context/context.js
@@ -7,7 +7,7 @@ const ProductContext = React.createContext();
 
 
 class ProductProvider extends Component{
-    state={
+    state = {
         sidbarOpen:false,
         cartOpen:false,
         soicalLinks:SoicalData,
@@ -64,13 +64,28 @@ setProducts = (products) =>{
         singleProduct:this.getStorageProduct(),
         loading:false
 
+    },
+    () =>{
+        //assign addTotal to set product function
+        this.addTotals()
     });
 
     // console.log(featuredProducts);
 } 
 // get cart from  local storage
 getStorageCart=() => {
-    return [];
+    //get saved cart from local storage 
+    // define temp cart 
+    let tempCart ;
+    //check if there is saved card in local storage 
+    if(localStorage.getItem('cart')){
+        tempCart = JSON.parse(localStorage.getItem(('cart')));
+    }
+    else{
+        tempCart = [];
+    }
+    //return the cart
+    return tempCart;
 }
 
 
@@ -83,11 +98,50 @@ getStorageProduct = ()=> {
 // cart items
 
 // get total 
-getTotal = ()=> {}
+getTotals = ()=> {
+    //define subtotal/count cart item
+    let subTotal = 0;
+    let cartItemCount = 0; 
+    //loop throw cart array
+    this.state.cart.forEach(item => {
+        //add cart sutotal/count
+        subTotal += item.total;
+        cartItemCount += item.counter;
+    }) 
+    subTotal = parseFloat(subTotal.toFixed(2));
+    //cal tax
+    let tax = subTotal * 0.1 ;
+    tax = parseFloat(tax.toFixed(2));
+    //cal tatal
+    let total = subTotal + tax;
+    total = parseFloat(total.toFixed(2));
+    //return an object
+    return {
+        subTotal,
+        tax,
+        cartItemCount,
+        total
+    };
+    // console.log(subTotal,cartItemCount,tax,total)
+}
 //add total
-addTotal = () => {}
+addTotals = () => {
+    //assign getTotal object to avariable
+    const totals = this.getTotals();
+    //replace the main cart object with new one
+    this.setState({
+        cartItems:totals.cartItemCount,
+        cartSubTotal:totals.subTotal,
+        cartTax:totals.tax,
+        cartTotal:totals.total,
+    })
+    // return to setProduct function to assign add totals to it
+}
 //sync storage
-syncStorage = () => {}
+syncStorage = () => {
+    // safe Main card to localStorage     
+    localStorage.setItem('cart',JSON.stringify(this.state.cart));
+}
 
 
 //add to cart
@@ -113,7 +167,7 @@ addToCart = id => {
         cart:tempCart
     },
         ()=>{
-        this.addTotal();
+        this.addTotals();
         this.syncStorage();
         this.openCart();
     });
