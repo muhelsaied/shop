19ff14f14b457dbfc390b169f202d81195a2ad72
MUHAMED ELSAIED <muhahmed2099@gmail.com>
2019-04-17 11:57:08 +0200

    fix button cart

--- src/components/cartPages/cartTotal.js
+++ src/components/cartPages/cartTotal.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ProductConsumer } from '../../context'
+import { Link } from 'react-router-dom'
 import TotalSub from './totalSub'
 export default function CartTotal() {
   return (
@@ -10,16 +11,21 @@ export default function CartTotal() {
           if (cart.length === 0 ){
             return <></>
           }
-          return( 
-            <div className='row m-0'>
-              <TotalSub text={`sub total : $ ${cartSubTotal}`}/>
-              <TotalSub text={`tax : $ ${cartTax}`}/>
-              <TotalSub  text={`total : $ ${cartTotal}`}/>
-                <button className='btn btn-outline-danger text-uppercase col-md-4 col-sm-4 mx-auto p-2 m-3'
-                  onClick={()=>clearCart()}>
-                  empty cart
-                </button>
-              </div>
+          return(
+              <div className='row m-0 mt-5'>
+                <TotalSub text={`sub total : $ ${cartSubTotal}`}/>
+                <TotalSub text={`tax : $ ${cartTax}`}/>
+                <TotalSub  text={`total : $ ${cartTotal}`}/>
+                  <button className='btn btn-outline-danger text-uppercase col-10 col-sm-4 col-lg-7 mx-auto p-2 m-3 sm-width-70'
+                    onClick={()=>clearCart()}>
+                    empty cart
+                  </button>
+                  <Link className='btn btn-outline-warning text-uppercase col-10 col-sm-4 col-lg-7 mx-auto p-2 m-3 sm-width-70'
+                  to ='/products'
+                    onClick={()=>clearCart()}>
+                    keep shopping
+                  </Link>
+                </div>
 
             )
 
